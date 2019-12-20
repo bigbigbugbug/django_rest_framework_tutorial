90b7117fc3bc421c3fc346a05ef7703cf51cd102
lijincheng <729453750@qq.com>
2019-12-20 11:37:06 +0800

    tutorial 部分代码

---
+++ snippets/urls.py
@@ -0,0 +1,47 @@
+from django.urls import path, include
+from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from snippets import views
+from snippets.views import SnippetViewSet, UserViewSet
+
+"""
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
+urlpatterns = [
+    path('', views.api_root),
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail'),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+"""
+routr = DefaultRouter()
+routr.register(r'snippets', views.SnippetViewSet)
+routr.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+    path('', include(routr.urls)),
+]
